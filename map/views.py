bcc4da5052994cfa93cc1389b231dcdc8b60b18f
suyandana <lalu.surya.jp@gmail.com>
2021-07-11 04:08:38 +0800

    First commit

--- map/views.py
+++ map/views.py
@@ -6,13 +6,11 @@ from .models import Coord
 def home(request):
     coord = Coord.objects.all()
     # coord_names = list()
-
     # for elem in coord:
     #     coord_names.append(", ".join([elem.name, elem.description, str(elem.lat), str(elem.lng)]))
-
     # response_html = '<br>'.join(coord_names)
-
     # return HttpResponse(response_html)
+    
     return render(request, 'map.html', {'coord': coord})
     
 def serialize(request):
